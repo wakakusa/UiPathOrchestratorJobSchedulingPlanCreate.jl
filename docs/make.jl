c8886525801db87d96127e6de479b0837fd537ee
wakakusa <wakakusaeng@outlook.jp>
2020-06-24 01:14:29 +0900

    modify document

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,19 @@
-using Documenter, UiPathOrchestratorJobSchedulingPlanCreate
+using UiPathOrchestratorJobSchedulingPlanCreate
+using Documenter
 
 makedocs(;
     modules=[UiPathOrchestratorJobSchedulingPlanCreate],
-    format=Documenter.HTML(),
-    pages=[
-        "Home" => "index.md",
-        "Getting Started" => "getting_started.md",
-        "Function" => "function.md",
-        "Excel" => "excel.md",
-    ],
+    authors="wakakusa",
     repo="https://github.com/wakakusa/UiPathOrchestratorJobSchedulingPlanCreate.jl/blob/{commit}{path}#L{line}",
     sitename="UiPathOrchestratorJobSchedulingPlanCreate.jl",
-    authors="wakakusa",
-    assets=String[],
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://wakakusa.github.io/UiPathOrchestratorJobSchedulingPlanCreate.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
 )
 
 deploydocs(;
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,18 +1,12 @@
-# UiPathOrchestratorJobSchedulingPlanCreate.jl
+```@meta
+CurrentModule = UiPathOrchestratorJobSchedulingPlanCreate
+```
 
-## これは何？
-Uipath Orchestatorで効率的なジョブ実行（必要最低限のロボットで実行）ができるようにジョブのタイムスケジュールを作成するプログラムです
+# UiPathOrchestratorJobSchedulingPlanCreate
 
-## インストール方法
-julia>]  
-(v1.1) add https://github.com/wakakusa/UiPathOrchestratorJobSchedulingPlanCreate.git  
-でプログラムがインストールされます。括弧内の数字は、利用しているバージョンが表示されます。
+```@index
+```
 
-## クイックガイド
-ファイル　schedule.xlsxをコピーして修正してください。  
-ExcelFilePathにはコピーしたschedule.xlsxまでのフルパスを指定してください
-
-uipathorchestratorschedulrecreate(InputFilePath,"parameters","schedule")
-を実行してください
-
-
+```@autodocs
+Modules = [UiPathOrchestratorJobSchedulingPlanCreate]
+```
